1713f752c8b929428309a96bbbf7432d05bd195c
bjbilel-02 <medbilelbjeoui@gmail.com>
2022-05-29 16:13:01 +0100

    Checkpoint JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import photo from "./imgs.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {/* <h1>HI I'M JSX</h1> */}
+      {/* <div className="all">
+        <div className="a">
+          <h3>welcom</h3>
+          <img src="./imgp.jpg" alt=""></img>
+        </div>
+        <div className="a">
+          <h3>welcom</h3>
+          <img src={photo} alt=""></img>
+        </div>
+      </div> */}
+      <div style={{border:"solid 1pxblack",maxWidth:"100vw"}}>
+
+<h1 class="title red">Your name here</h1>
+
+<br />
+
+<img src={photo} alt=""/>
+
+<br />
+
+<img src="/imageInPublic.jpg" alt="" />
+
+</div>
+
+<video width="320" height="240" controls>
+
+<source src="myVideo.mp4" type="video/mp4" />
+
+</video>
+
+   </div>
+      
+
   );
 }
 
 export default App;
+
